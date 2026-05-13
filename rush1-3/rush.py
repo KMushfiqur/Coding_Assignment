258191496e3d7feb170d31ddb09cb49125f2023e
KMushfiqur <khan.mushfiqur21@gmail.com>
2026-05-12 22:06:31 -0700

    adding Assignment 3

---
+++ rush1-3/rush.py
@@ -0,0 +1,40 @@
+import sys
+
+def rush(x, y):
+    if x <= 0 or y <= 0:
+        print("Invalid size", file=sys.stderr)
+        return
+
+    for row in range(y):
+        draw = ""
+
+        for col in range(x):
+            # Corners
+            
+            if (row == 0 ) and (col == 0 or col == x - 1):
+                if(x == 1 or y == 1):
+                    draw+= "B"
+                    continue
+                draw+= "A"
+
+            elif (row == y-1 and (col == x - 1 or col == 0)):
+                if(x == 1 or y == 1):
+                    draw+= "B"
+                    continue
+                draw += "C"
+                
+            # borders
+            elif (row == 0 or row == y - 1 or col == 0 or col == x - 1):
+                draw += "B"
+
+            # Inside
+            else:
+                draw += " "
+
+        print(draw)
+
+#Test
+##rush(5,3);
+##rush(5,1);
+##rush(1,3);
+##rush(6,4);
